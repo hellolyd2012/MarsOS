5c1a36b34eb04ea6cb13189713690a1ad4aa3f33
lyd <lyd20122024@outlook.com>
2024-11-09 08:34:52 +0800

    Update

--- Arch/X86/Boot/Boot.c
+++
@@ -1,7 +0,0 @@
-// 创建于2024.11.8
-// MarsOS X86 Boot代码
-// 其源码遵循GNU-LGPl V3.0
-
-void _Init()
-{  
-}
--- Includes/Boot/MultiBoot.h
+++
@@ -1,9 +0,0 @@
-// 创建于2024.11.9
-// MarsOS MultiBoot规范定义
-// 其源码遵循GNU-LGPl V3.0
-
-#ifndef MarsOS_MultiBoot_H
-#define MAGIC 0x1BADB002
-#define ALI 0x03
-#define CHECKSUM(CHECK)     -(MAGIC + CHECK)
-#endif
