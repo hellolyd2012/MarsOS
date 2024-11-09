91560abb6665db99fbe1d507f2d59a49e303cbe9
lyd <lyd20122024@outlook.com>
2024-11-09 13:19:13 +0800

    MultiBoot

---
+++ Includes/Boot/MultiBoot.h
@@ -0,0 +1,10 @@
+// 创建于2024.11.9
+// MarsOS MultiBoot
+
+#define MAGIC 0x1BADB002
+#define FLAGS 7
+#define CHECKSUM    -(MAGIC + FLAGS)
+#define ModeType 0
+#define Width 1280
+#define Height 720
+#define Depth 32
